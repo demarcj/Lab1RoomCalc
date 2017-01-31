cfed4c4227121f21592c305522dde8a09940f8bf
Demarcj <demarcjohnson@yahoo.com>
2017-01-31 16:27:38 -0500

    Finish

--- src/RoomPerimeter.java
+++ src/RoomPerimeter.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
 /**
- * Created by DeMarcj on 1/30/2017.
+ * Created by DeMarc Johnson
+ * 1/30/2017
  */
 public class RoomPerimeter {
     public static void main(String [] args){
         System.out.println("Welcome to Grand Circus\' Room Detail Generator!");
+
+        //Basic reference for the while loop
         Scanner scan = new Scanner(System.in);
         String text = "";
+
+        //This do while loop allow the user to restart the program if not satisfy with the answer
         do {
             System.out.print("Enter Length: ");
             double length = scan.nextDouble();
@@ -15,8 +20,9 @@ public class RoomPerimeter {
             System.out.print("Enter Width: ");
             double width = scan.nextDouble();
 
-            final double area = length * width;
-            final double perimeter = length * 2 + width * 2;
+            double area = length * width;
+            double perimeter = length * 2 + width * 2;
+
             System.out.println("Area: " + area);
             System.out.println("Perimeter: " + perimeter);
             System.out.println("");
@@ -24,6 +30,7 @@ public class RoomPerimeter {
             text = scan.next();
         }
         while(text.equals("y"));
+        System.out.print("Goodbye")
         scan.close();
     }
 }
